af27362995708bcbd0184442cefaa2046c4c77a2
suzeneoo <sujin102503@gmail.com>
2024-02-24 15:45:24 +0900

    test yoon

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/material.dart';
 import 'stage.dart';
 
+//yoyooyoyyyoy
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: MyAppScreen());
+    return MaterialApp(home: MyAppScreen());
   }
 }
+
 class MyAppScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewStage()),
-            );
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/bg_home_test.png"),
-                fit: BoxFit.cover,
-              ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => NewStage()),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bg_home_test.png"),
+              fit: BoxFit.cover,
             ),
-            
           ),
         ),
-      );
+      ),
+    );
   }
 }
 
