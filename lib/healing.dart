683108b70aadd2101becd95a3cddfebf82e3628d
suzeneoo <sujin102503@gmail.com>
2024-03-06 13:58:50 +0900

    자장가, 힐링 수정

--- lib/healing.dart
+++ lib/healing.dart
@@ -21,31 +21,31 @@ class _HealingGameScreenState extends State<HealingGameScreen> {
   bool areWidgetsOverlapping = false;
   bool isShowingNotification = false;
 
-  double hookLeft = 300;
-  double hookTop = 400;
+  double hookLeft = 300; // 낚싯대의 초기 왼쪽 위치
+  double hookTop = 400; // 낚싯대의 초기 위쪽 위치
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/bg_stage.png"),
-                fit: BoxFit.cover,
-              ),
+        body: Stack(
+      children: <Widget>[
+        // 배경 이미지를 표시하는 컨테이너
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bg_stage.png"),
+              fit: BoxFit.cover,
             ),
           ),
-        Positioned(
-          
-          child: Center(
-            child: Image.asset(
-            'assets/images/mooner.png',
-            width: 300,
-            height: 300,
-            fit: BoxFit.cover)),
         ),
+        // 중앙에 플레이어 이미지를 표시
+        Positioned(
+          child: Center(
+              child: Image.asset('assets/images/mooner.png',
+                  width: 350, height: 350, fit: BoxFit.cover)),
+        ),
+
+        // 드래그 가능한 낚싯대 이미지를 표시
         Positioned(
           width: 70,
           height: 70,
@@ -66,7 +66,7 @@ class _HealingGameScreenState extends State<HealingGameScreen> {
             //크기 유지
             feedback: Material(
               child: Image.asset(
-                'assets/images/hook.png',
+                'assets/images/hook.png', //드래그 중인 이미지
                 width: 70,
                 height: 70,
                 fit: BoxFit.cover,
@@ -76,42 +76,45 @@ class _HealingGameScreenState extends State<HealingGameScreen> {
               // 드래그가 종료될 때 위치 업데이트하지 않음
               checkOverlap();
             },
-            childWhenDragging: Container(),
+            childWhenDragging: Container(), // 드래그 중에 대체할 위젯
           ),
         ),
       ],
-     ) );
+    ));
   }
-  
+
+  // 겹치는 지 확인하는 메소드
   void checkOverlap() {
     RenderBox renderBox = context.findRenderObject() as RenderBox;
     Offset position = renderBox.localToGlobal(Offset(hookLeft, hookTop));
 
     setState(() {
+      // 겹치는 지 여부를 확인하고 상태 업데이트
       //print(position.dx);
       print(position.dy);
       // 겹치는지 확인
-      areWidgetsOverlapping = position.dx >= MediaQuery.of(context).size.width - 100 &&
-          position.dy >= 400 &&
-          //position.dx <= MediaQuery.of(context).size.width &&
-          position.dy <= 1000;
+      areWidgetsOverlapping =
+          position.dx >= MediaQuery.of(context).size.width - 100 &&
+              position.dy >= 400 &&
+              //position.dx <= MediaQuery.of(context).size.width &&
+              position.dy <= 1000;
     });
 
     if (!areWidgetsOverlapping && !isShowingNotification) {
-      // 안겹친 경우 알림 표시
+      // 겹치지 않고 알림을 표시하지 않았을 경우 알림 표시
       showNotification();
-    }
-
-    else {
+    } else {
+      // 겹치거나 이미 알림을 표시한 경우 초기 위치로 재설정
       hookLeft = 300;
       hookTop = 400;
     }
   }
 
+  // 알림을 표시하는 메소드
   void showNotification() {
     setState(() {
-       isShowingNotification = true;
-     });
+      isShowingNotification = true;
+    });
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -127,7 +130,6 @@ class _HealingGameScreenState extends State<HealingGameScreen> {
                 });
               },
               child: Text('다음'),
-              
             ),
           ],
         );
