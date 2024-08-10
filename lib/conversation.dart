1971fc584454bea55ce59fdfb1773728dc7f0ade
jjamk <rkdtjgus1120@naver.com>
2024-08-10 23:50:39 +0900

    대화하기 및 별세개 바 제작

--- lib/conversation.dart
+++ lib/conversation.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:mooner_interface/stage.dart';
 import 'package:simple_animation_progress_bar/simple_animation_progress_bar.dart';
+
 void main() => runApp(ConversationGameApp());
 
 class ConversationGameApp extends StatelessWidget {
@@ -11,48 +10,7 @@ class ConversationGameApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'BMJUA'),
-      home: InitialDialogueScreen(),
-      //home: ConversationGameScreen(),
-    );
-  }
-}
-
-class InitialDialogueScreen extends StatefulWidget {
-  @override
-  _InitialDialogueScreenState createState() => _InitialDialogueScreenState();
-}
-
-class _InitialDialogueScreenState extends State<InitialDialogueScreen> {
-  // 대화 인덱스와 대화 내용
-  int dialogueIndex = 0;
-  List<String> dialogues = [
-    "너무너가 하고싶은 말이 있어 보여.\n 나 전달법을 사용해서 말할 수 있도록 도와주자.",
-  ];
-
-  void nextDialogue() {
-    setState(() {
-      if (dialogueIndex < dialogues.length - 1) {
-        dialogueIndex++;
-      } else {
-        // 모든 대화가 완료되었을 때, 게임 화면으로 전환
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ConversationGameScreen()));
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(dialogues[dialogueIndex], style: TextStyle(fontSize: 24)),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: nextDialogue, child: Text("시작하기")),
-          ],
-        ),
-      ),
+      home: ConversationGameScreen(),
     );
   }
 }
@@ -64,165 +22,56 @@ class ConversationGameScreen extends StatefulWidget {
 
 class _ConversationGameScreenState extends State<ConversationGameScreen> {
   int score = 0;
-  Timer? _timer;
-  final int _timeLimit = 30; // 30초 제한 시간
   double _progress = 1.0;
-  double ratio = 0;
+  int currentIndex = 0;
+  int dialogueIndex = 0;
+  bool isDialogueComplete = false;
+
+  List<String> dialogues = [
+    '어부\n     문찌가 하고싶은 말이 있어보여. \n     적절한 말하기를 하도록 도와줘!',
+  ];
+
+  void nextDialogue() {
+    setState(() {
+      if (dialogueIndex < dialogues.length - 1) {
+        dialogueIndex++;
+      } else {
+        isDialogueComplete = true;
+      }
+    });
+  }
 
   List<ConversationItem> conversation = [
     ConversationItem(
       speaker: 'Character A',
       text: 'stage1',
       choices: [
-        Choice(text: '이 말미잘아!', nextIndex:1, score:-1 ),
-        Choice(text: '(심호흡을 한다)', nextIndex:1, score: 1),
+        Choice(text: '이 말미잘아!', nextIndex: 1, score: -1),
+        Choice(text: '(심호흡을 한다)', nextIndex: 1, score: 1),
       ],
     ),
     ConversationItem(
       speaker: 'Character A',
       text: 'stage2',
       choices: [
-        Choice(text: '이 해삼 똥구멍아!!', nextIndex:2,score: -2),
-        Choice(text: '나 너무너무 화났어!', nextIndex:2,score: 1),
+        Choice(text: '이 해삼 똥구멍아!!', nextIndex: 2, score: -2),
+        Choice(text: '나 너무너무 화났어!', nextIndex: 2, score: 1),
       ],
     ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage3',
-      choices: [
-        Choice(text: '너 내말 똑똑히 들어!', nextIndex:3,score: -2),
-        Choice(text: '나는 우리가 대화가 필요하다고 생각해', nextIndex:3,score: 1),
-
-      ],
-    ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage4',
-      choices: [
-        Choice(text: '너는 진짜 너무해..', nextIndex:4,score: -1),
-        Choice(text: '나는 너가 진짜 너무했다는 생각이 들어', nextIndex:4,score: 1),
-      ],
-    ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage5',
-      choices: [
-        Choice(text: '너는 항상 그런식으로 말해! ', nextIndex:5,score: -3),
-        Choice(text: '너 왜 그런식으로 말해?', nextIndex:5,score: -2),
-        Choice(text: '나는 너가 그런식으로 말하면 화가나!', nextIndex:5,score: 1),
-      ],
-    ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage6',
-      choices: [
-        Choice(text: '너는 나쁜사람이야! ', nextIndex:6,score: -3),
-        Choice(text: '너의 그런 행동은 나빠!', nextIndex:6,score: -2),
-        Choice(text: '나는 너가 나쁜사람이라는 생각이 들었어.', nextIndex:6,score: -1),
-        Choice(text: '나는 너의 그런 행동이 나쁘다고 생각해.', nextIndex:6,score: 2),
-      ],
-    ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage7',
-      choices: [
-        Choice(text: '다시는 그딴식으로 말하지 마!  ', nextIndex:7,score: -3),
-        Choice(text: '너 그렇게 말하면 안돼.', nextIndex:7,score: -2),
-        Choice(text: '나는 너가 그렇게 말하지 않았으면 좋겠어', nextIndex:7,score: 1),
-      ],
-    ),
-    ConversationItem(
-      speaker: 'Character A',
-      text: 'stage7',
-      choices: [
-        Choice(text: '다시는 그딴식으로 말하지 마!  ', nextIndex:7,score: -3),
-        Choice(text: '너 그렇게 말하면 안돼.', nextIndex:7,score: -2),
-        Choice(text: '나는 너가 그렇게 말하지 않았으면 좋겠어', nextIndex:7,score: 1),
-      ],
-    ),
+    // 나머지 대화 내용은 그대로 유지...
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    _startTimer();
-  }
-
-  void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        ratioVal();
-        _progress -= 1 / _timeLimit; // 매 초마다 진행률 감소
-
-        if (_progress <= 0) {
-          _timer?.cancel();
-          _showScoreDialog();
-        }
-      });
-    });
-  }
 
   void _handleChoice(Choice choice) {
     setState(() {
       score += choice.score;
       currentIndex = choice.nextIndex;
 
-      if (currentIndex == conversation.length-1) {
-      // 대화가 끝났을 때
-      _timer?.cancel();
-      _showScoreDialog();
-    } 
+      if (currentIndex == conversation.length - 1) {
+        // 대화가 끝났을 때
+        _showScoreDialog();
+      }
     });
   }
-
-  
-
-
-  @override
-  void dispose() {
-    _timer?.cancel(); // 타이머 정리
-    super.dispose();
-  }
-
-
-  void ratioVal() {
-    if (ratio == 0) {
-      Future.delayed(const Duration(seconds: 2), () {
-        setState(() {
-          ratio = 0.2;
-        });
-        if (ratio == 0.2) {
-          Future.delayed(const Duration(seconds: 1), () {
-            setState(() {
-              ratio = 0.4;
-            });
-            if (ratio == 0.4) {
-              Future.delayed(const Duration(seconds: 1), () {
-                setState(() {
-                  ratio = 0.7;
-                });
-                if (ratio == 0.7) {
-                  Future.delayed(const Duration(seconds: 1), () {
-                    setState(() {
-                      ratio = 1;
-                    });
-                    if (ratio == 1) {
-                      Future.delayed(const Duration(seconds: 1), () {
-                        setState(() {
-                          ratio = 0;
-                        });
-                      });
-                    }
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-  }
-  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -231,103 +80,185 @@ class _ConversationGameScreenState extends State<ConversationGameScreen> {
         child: Stack(
           children: <Widget>[
             Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/bg_stage.png"),
-                fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/bg_stage.png"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          //상단 스테이지번호
-          Positioned(
+            // 상단 스테이지번호
+            Positioned(
               left: 0,
               top: 0,
               child: Image.asset(
                 'assets/images/stage_background.png',
-                width: 150, 
+                width: 150,
                 height: 150,
               ),
             ),
             Positioned(
-              left: 36, 
+              left: 36,
               top: 65,
               child: Text(
                 '#stage 9',
                 style: TextStyle(
-                  fontSize: 18, 
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black, 
+                  color: Colors.black,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  
-                  Text(
-                    'Score: $_timer',
-                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Score: $score',
-                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    conversation[currentIndex].speaker,
-                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    conversation[currentIndex].text,
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(height: 16.0),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: conversation[currentIndex].choices.length,
-                    itemBuilder: (context, index) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          _handleChoice(conversation[currentIndex].choices[index]);
-                        },
-                        child: Text(
-                          conversation[currentIndex].choices[index].text,
-                        ),
-                      );
-                    },
-                  ),
-                SimpleAnimationProgressBar(
-                height: 300,
-                width: 15,
-                backgroundColor: Colors.grey,//Colors.grey.shade800,
-                foregrondColor: Colors.red,
-                ratio: _progress,
-                direction: Axis.vertical,
-                curve: Curves.fastLinearToSlowEaseIn,
-                duration: const Duration(seconds: 1),
-                borderRadius: BorderRadius.circular(10),
-                gradientColor: const LinearGradient(
-                    colors: [Colors.red, Colors.orange],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter),
-              ),
-                  // LinearProgressIndicator(
-                  //   value: _progress, // 진행률에 따라 막대 길이 변경
-                  //   backgroundColor: Colors.grey, // 배경색
-                  //   valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // 막대색
-                  // ),
-                ],
+            // 별 프로그레스 바
+            Positioned(
+              left: 56,
+              top: 200,
+              child: Center(
+                child: Stack(
+                  clipBehavior: Clip.none, // Positioned가 Stack 바깥으로 나가도록 허용
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      child: SimpleAnimationProgressBar(
+                        height: 30,
+                        width: 330,
+                        backgroundColor: Colors.grey,
+                        foregrondColor: Colors.red,
+                        ratio: _progress,
+                        direction: Axis.horizontal,
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        duration: const Duration(seconds: 1),
+                        borderRadius: BorderRadius.circular(10),
+                        gradientColor: const LinearGradient(
+                            colors: [Colors.red, Colors.orange],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight),
+                      ),
+                    ),
+                    Positioned(
+                      left: -45,
+                      top: -40, // 별 이미지를 약간 위로 올리기 위해 top 값 조정
+                      child: Image.asset(
+                        'assets/images/star.png', // 별 이미지 경로
+                        width: 110,
+                        height: 110,
+                      ),
+                    ),
+                    Positioned(
+                      left: 105,
+                      top: -40, // 중앙 별 이미지의 위치
+                      child: Image.asset(
+                        'assets/images/star.png', // 별 이미지 경로
+                        width: 110,
+                        height: 110,
+                      ),
+                    ),
+                    Positioned(
+                      right: -45,
+                      top: -40, // 오른쪽 끝 별 이미지의 위치
+                      child: Image.asset(
+                        'assets/images/star.png', // 별 이미지 경로
+                        width: 110,
+                        height: 110,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          
-          ]),
+            // 중앙에 문어 이미지
+            Positioned(
+              child: Center(
+                child: Image.asset('assets/images/normal_mooner_x.png',
+                    width: 400, height: 320, fit: BoxFit.contain),
+              ),
+            ),
+            if (!isDialogueComplete)
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: GestureDetector(
+                  onTap: nextDialogue,
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/fisher.png',
+                          width: 130,
+                          height: 130,
+                          fit: BoxFit.cover,
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(47),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(
+                                    "assets/images/dialog_background.png"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Text(
+                              dialogues[dialogueIndex],
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.white),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            if (isDialogueComplete)
+              Padding(
+                padding: EdgeInsets.all(2.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      '점수: $score',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      conversation[currentIndex].speaker,
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      conversation[currentIndex].text,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: conversation[currentIndex].choices.length,
+                      itemBuilder: (context, index) {
+                        return ElevatedButton(
+                          onPressed: () {
+                            _handleChoice(conversation[currentIndex]
+                                .choices[index]);
+                          },
+                          child: Text(
+                            conversation[currentIndex].choices[index].text,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+          ],
         ),
-      );
+      ),
+    );
   }
-  
+
   void _showScoreDialog() {
     showDialog(
       context: context,
@@ -339,9 +270,9 @@ class _ConversationGameScreenState extends State<ConversationGameScreen> {
             TextButton(
               onPressed: () {
                 Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NewStage()),
-                      );
+                  context,
+                  MaterialPageRoute(builder: (context) => NewStage()),
+                );
               },
               child: Text('OK'),
             ),
@@ -357,7 +288,11 @@ class ConversationItem {
   final String text;
   final List<Choice> choices;
 
-  ConversationItem({required this.speaker, required this.text, required this.choices});
+  ConversationItem({
+    required this.speaker,
+    required this.text,
+    required this.choices,
+  });
 }
 
 class Choice {
@@ -365,5 +300,9 @@ class Choice {
   final int nextIndex;
   final int score;
 
-  Choice({required this.text, required this.nextIndex, required this.score});
+  Choice({
+    required this.text,
+    required this.nextIndex,
+    required this.score,
+  });
 }
